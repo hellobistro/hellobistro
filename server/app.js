c21dab8f1cc1f63bfa7896c0c86dcd10907baddb
Spencer <spencer0cornelia@gmail.com>
2018-04-09 20:14:12 -0700

    added app.js server file & added package.json & added startup dependencies for express server

---
+++ server/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, '*ENTER DIR NAME OF STATIC FILES*')));
+
+app.listen(port, () => {
+  console.log(`Starting the server at port ${port}`);
+});
