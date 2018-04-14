8d6771a684d0522393ead21a06de496a1953d02b
Chris Wohlers <chriswohlers@gmail.com>
2018-04-14 10:49:48 -0700

    config change.

--- database/config.js
+++ database/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   database: 'hellobistro',
   username: 'hbroot',
-  password: 'hBdBhello',
+  password: 'test',
   host: 'hellodb.cbwzzgue2vgj.us-east-2.rds.amazonaws.com',
   port: '3306',
 }
