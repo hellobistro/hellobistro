9a74888da08b09e413b135375f0ae627696cada0
Fat Pikachu <fatpikachu@Fats-MacBook-Pro.local>
2018-04-13 23:11:00 -0700

    basic redux

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,7 +1,11 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
-
+import posts from './testReducer'
 //import all reducers, combine them
-const rootReducer = combineReducers({routing: routerReducer });
+// const rootReducer = combineReducers({routing: routerReducer });
 
-export default rootReducer;
+
+
+export const testReducer = combineReducers({posts, routing: routerReducer})
+
+// export default rootReducer;
--- client/src/store.js
+++ client/src/store.js
@@ -2,7 +2,7 @@ import { createStore, compose} from 'redux';
 import { syncHistoryWithStore } from 'react-router-redux';
 import { browserHistory } from 'react-router';
 
-import rootReducer from './reducers/index'
+import {testReducer} from './reducers/index'
 
 
 
@@ -10,8 +10,9 @@ const defaultState = {
   
 }
 
-const store = createStore(rootReducer, defaultState);
+//const store = createStore(rootReducer, defaultState);
+const store = createStore(testReducer, defaultState);
 
-export const history = syncHistoryWithStore(browserHistory, store);
+export const history = {} //syncHistoryWithStore(browserHistory, store);
 
 export default store;
