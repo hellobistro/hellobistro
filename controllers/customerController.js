33ed43d0c094050d16cf3203dd141d4cc0ec840b
Chris Poole <poolecs@gmail.com>
2018-04-18 13:35:30 -0700

    Implement create/delete for Customer, Restaurant

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -2,7 +2,40 @@ const { Customer, MenuItem }  = require('../database/index.js');
 
 const customerController = {
 
-  createCustomer() {},
+  createCustomer(req, res) {
+    const {
+      userName,
+      firstName,
+      lastName,
+      password,
+      zip,
+      phone,
+      email,
+      availVotes,
+      paymentId,
+      vendor,
+      apiKey,
+    } = req.body;
+
+    Customer.create({
+      userName,
+      firstName,
+      lastName,
+      password,
+      zip,
+      phone,
+      email,
+      availVotes,
+      paymentId,
+      vendor,
+      apiKey,
+    }).then((customer) => {
+      res.status(201).json(customer);
+    }).catch((err) => {
+      console.log(err);
+      res.send(err);
+    });
+  },
 
   getAllCustomers(req, res) {
     Customer.findAll({include: [{model: MenuItem}]}).then((customers) => {
@@ -24,7 +57,11 @@ const customerController = {
         required: false,
       }],
     }).then((customer) => {
-      res.send(customer);
+      if (customer === null) {
+        res.sendStatus(400);
+      } else {
+        res.status(200).json(customer);
+      }
     }).catch((err) => {
       res.send(err);
     });
@@ -34,7 +71,22 @@ const customerController = {
 
   loginCustomer() {},
 
-  deleteCustomer() {},
+  deleteCustomer(req, res) {
+    const { customer_id } = req.params;
+
+    Customer.destroy({
+      where: { id: customer_id },
+    }).then((deleted) => {
+      if (deleted < 1) {
+        res.sendStatus(400);
+      } else {
+        res.sendStatus(200);
+      }
+    }).catch((err) => {
+      console.log('err', err);
+      res.send(err);
+    });
+  },
 
 };
 
