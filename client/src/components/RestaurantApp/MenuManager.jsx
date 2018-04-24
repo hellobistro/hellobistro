e712c27de8c39ec6415081789cb7219007d3a91b
Chris Wohlers <chriswohlers@gmail.com>
2018-04-24 15:22:07 -0700

    Change MenuManager component to be class.

--- client/src/components/RestaurantApp/MenuManager.jsx
+++ client/src/components/RestaurantApp/MenuManager.jsx
@@ -1,11 +1,28 @@
 // Import dependencies
 import React from 'react';
+import MenuSection from './MenuSection';
+import { sampleRestaurantGet } from '../../../sampleData';
 
+class MenuManager extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      editItem: false,
+      data: sampleRestaurantGet,
+    };
+  }
 
-const MenuManager = () => (
-  <div className="MenuManager DebugComponentBlue">
-    <p>This is the <strong>MenuManager</strong> component</p>
-  </div>
-);
+  render() {
+    const menuSections = this.state.data.MenuSections.map(section => <MenuSection data={section} />);
+
+    return (
+      <div className="MenuManager DebugComponentBlue">
+        <p>This is the <strong>Menu Manager</strong> component</p>
+        {menuSections}
+        <button>Add item</button><button>Publish all</button>
+      </div>
+    );
+  }
+}
 
 export default MenuManager;
