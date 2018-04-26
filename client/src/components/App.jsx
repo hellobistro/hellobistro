8a8598096f421ab53d2f51f2748d2418a781f648
Chris Wohlers <chriswohlers@gmail.com>
2018-04-25 20:53:00 -0700

    Retrieve mock Restaurant data from componentdidmount

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -8,52 +8,60 @@ import { CustomerAppContainer } from './Containers';
 // Import RestaurantApp component and subcomponents
 import { RestaurantAppContainer } from './Containers';
 
+// Import sample data to mock restaurant get request
+import { sampleRestaurantGet } from '../../sampleData';
+
 // Create parent application
 class App extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        testAsCustomer: true,
-        testAsRestaurant: true,
-      };
-    }
-  
-    toggleTestAsCustomer() {
-      this.setState({
-        testAsCustomer: !this.state.testAsCustomer,
-      });
-    }
-  
-    toggleTestAsRestaurant() {
-      this.setState({
-        testAsRestaurant: !this.state.testAsRestaurant,
-      });
-    }
-  
-    render() {
-      console.log('the props~~~ in App', this.props)
-      return (
-        <div>
-          <h1>Welcome to HelloBistro</h1>
-
-          <h2>The below buttons are for testing only</h2>
-          <button onClick={() => { this.toggleTestAsCustomer(); }}>
-            Show customer components
-          </button>
-          <button onClick={() => { this.toggleTestAsRestaurant(); }}>
-            Show restaurant components
-          </button>
-          <button onClick={() => { this.props.increment(); }}>
-            trigger
-          </button>
-  
-          { this.state.testAsCustomer ? <Route path="/" component={CustomerAppContainer} /> : <div /> }
-          { this.state.testAsRestaurant ? <Route path="/" component={RestaurantAppContainer} /> : <div /> }
-  
-        </div>
-      );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      testAsCustomer: true,
+      testAsRestaurant: true,
+    };
   }
-  
-  export default App;
-  
+
+  toggleTestAsCustomer() {
+    this.setState({
+      testAsCustomer: !this.state.testAsCustomer,
+    });
+  }
+
+  toggleTestAsRestaurant() {
+    this.setState({
+      testAsRestaurant: !this.state.testAsRestaurant,
+    });
+  }
+
+  componentDidMount() {
+    // Mocking get to /restaurants/:restaurantId
+    this.props.getRestaurantData(sampleRestaurantGet);
+  }
+
+  render() {
+    console.log('the props~~~ in App', this.props);
+    return (
+      <div>
+        <h1>Welcome to HelloBistro</h1>
+
+        <h2>The below buttons are for testing only</h2>
+        <button onClick={() => { this.toggleTestAsCustomer(); }}>
+            Show customer components
+        </button>
+        <button onClick={() => { this.toggleTestAsRestaurant(); }}>
+            Show restaurant components
+        </button>
+        <button onClick={() => { this.props.increment(); }}>
+            trigger
+        </button>
+
+        { this.state.testAsCustomer ? <Route path="/" component={CustomerAppContainer} /> : <div /> }
+        { this.state.testAsRestaurant ? <Route path="/" component={RestaurantAppContainer} /> : <div /> }
+
+      </div>
+    );
+  }
+}
+
+export default App;
+
