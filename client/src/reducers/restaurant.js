685c561d7505c7eea88cdbfd3d26d0c3e1618576
Chris Wohlers <chriswohlers@gmail.com>
2018-04-25 20:54:31 -0700

    Add restaurant get to redux store.

--- client/src/reducers/restaurant.js
+++ client/src/reducers/restaurant.js
@@ -1,6 +1,20 @@
-function items(state = [], action) {
-  console.log('REDUX', state, action);
-  return state;
+const initialState = {
+  data: {
+    MenuSections: [],
+  },
+};
+
+function restaurant(state = initialState, action) {
+  switch (action.type) {
+    case 'GET_RESTAURANT_DATA':
+      return Object.assign({}, state, {
+        data: action.data,
+      });
+    default:
+      return state;
+  }
+  /* * commenting out duplicate 'return state' * */
+  // return state;
 }
 
-export default items;
+export default restaurant;
--- client/src/store.js
+++ client/src/store.js
@@ -1,17 +1,13 @@
-import { createStore, compose} from 'redux';
+import { createStore, compose } from 'redux';
 import { syncHistoryWithStore } from 'react-router-redux';
 import { browserHistory } from 'react-router';
 
-import rootReducer from './reducers/index'
-
-
+import rootReducer from './reducers/index';
 
 const defaultState = {
-  
-}
+};
 
-
-const store = createStore(rootReducer, defaultState);
+const store = createStore(rootReducer, defaultState, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 export const history = {} //syncHistoryWithStore(browserHistory, store);
 
