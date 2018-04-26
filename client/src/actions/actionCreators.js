41592bbb875ea675608daaf5ac4f5228dc72c2f1
Chris Wohlers <chriswohlers@gmail.com>
2018-04-25 20:52:30 -0700

    Add action for restaurant get.

--- client/src/actions/actionCreators.js
+++ client/src/actions/actionCreators.js
@@ -1,10 +1,17 @@
 export function increment() {
-    return {
-      type: 'INCREMENT_LIKES'
-    }
-  }
+  return {
+    type: 'INCREMENT_LIKES',
+  };
+}
 
-  export function addCustomer(obj){
-    obj.type = 'ADD_CUSTOMER'
-    return obj;
-  }
+export function addCustomer(obj) {
+  obj.type = 'ADD_CUSTOMER';
+  return obj;
+}
+
+export function getRestaurantData(data) {
+  return {
+    type: 'GET_RESTAURANT_DATA',
+    data,
+  };
+}
