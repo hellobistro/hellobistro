b54967f6fae3b3fbe1aa662c1694eda0244e3c59
Chris Poole <poolecs@gmail.com>
2018-04-28 13:32:47 -0700

    Store state upon reload

--- client/src/store.js
+++ client/src/store.js
@@ -2,13 +2,18 @@ import { createStore, compose } from 'redux';
 import { syncHistoryWithStore } from 'react-router-redux';
 import { browserHistory } from 'react-router';
 import { createBrowserHistory } from 'history';
-
 import rootReducer from './reducers/index';
+import { saveState, loadState } from './services/LocalStorage.js';
 
 const defaultState = {
 };
 
-const store = createStore(rootReducer, defaultState, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const persistedState = loadState();
+const store = createStore(rootReducer, persistedState, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+store.subscribe(() => {
+  saveState(store.getState());
+});
 
 export const history = syncHistoryWithStore(createBrowserHistory(), store);
 //export const history = syncHistoryWithStore(browserHistory, store);
