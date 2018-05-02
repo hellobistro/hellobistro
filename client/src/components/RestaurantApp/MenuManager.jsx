3c22752dec310d53bd9d9dd36520971561e02aee
Fat Pikachu <fatpikachu@Fats-MacBook-Pro.local>
2018-05-01 17:40:32 -0700

    menumanger handles changes in frontend and saved to react state

--- client/src/components/RestaurantApp/MenuManager.jsx
+++ client/src/components/RestaurantApp/MenuManager.jsx
@@ -2,32 +2,75 @@
 import React from 'react';
 import MenuSection from './MenuSection';
 import { sampleRestaurantGet } from '../../../sampleData';
-import MenuEditItem from './MenuEditItem';
 
 class MenuManager extends React.Component {
   constructor() {
     super();
-    this.state = {
-      editItem: false,
-    };
+    this.state = {};
   }
 
-  toggleEdit = (editData) => {
-    this.setState({ 
-      editItem: !this.state.editItem,
-      editData,
-    });
-    console.log('toggled edit')
+  componentWillMount() {
+    //api call
+    //sampleRestaurantGet//obj
+    this.setState({ ...sampleRestaurantGet })
+  }
+
+  deleteMenuSection = (index) => {
+    let newMenuSections = this.state.MenuSections;
+    newMenuSections.splice(index, 1);
+    this.setState({MenuSections: newMenuSections});
+  }
+
+  inputChange = (sectionIndex, itemIndex, e) => {
+    let menuSections = this.state.MenuSections;
+    let name = e.target.name;
+    let value;
+    if (name === "name" || name === "status") {
+      value = e.target.value
+    } else if(typeof JSON.parse(e.target.value) === "number" || typeof JSON.parse(e.target.value) === "boolean") {
+      value = JSON.parse(e.target.value)
+    } else {
+      value = null;
+    }
+    menuSections[sectionIndex].MenuItems[itemIndex][name] = value;
+    if (name !== "status"){
+      menuSections[sectionIndex].MenuItems[itemIndex].status = "draft";
+    }
+    this.setState({
+      MenuSections: menuSections
+    })
+  }
+
+  addItem = (sectionIndex) => {
+    let itemTemplate = {
+      name: null,
+      price: null,
+      vegan: null,
+      vegetarian: null,
+      glutenFree: null,
+      spicy: null,
+      prepTime: null
+    }
+    let newMenuSections = this.state.MenuSections
+    newMenuSections[sectionIndex].MenuItems.push(itemTemplate)
+    this.setState({MenuSections: newMenuSections});
+  }
+  
+  clickSave(){
+    
   }
 
   render() {
-    const menuSections = this.props.state.restaurant.data.MenuSections.map(section => <MenuSection data={section} toggleEdit={this.toggleEdit}/>);
-
+    console.log('the state inside menuManager#####', this.state)
+    const menuSections = sampleRestaurantGet.MenuSections.map((section, i) => 
+    <MenuSection key={i} sectionIndex={i} data={section} dropDown={this.dropDown} 
+      inputChange={this.inputChange} deleteMenuSection={this.deleteMenuSection} 
+      addItem={this.addItem} />);
     return (
-      <div className="MenuManager">
+      <div className="MenuManager form">
         {menuSections}
-        <button>Add item</button><button>Publish all</button>
-        <MenuEditItem view={this.state.editItem} data={this.state.editData} toggleEdit={this.toggleEdit}/>
+        <button>Add item</button><button onClick={this.clickSave}>Save Changes</button>
+        <button>Add a menu section</button>
       </div>
     );
   }
--- client/src/components/RestaurantApp/MenuSection.jsx
+++ client/src/components/RestaurantApp/MenuSection.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import MenuItem from './MenuItem';
 
-const MenuSection = ({ data, toggleEdit }) => {
+const MenuSection = ({ data, deleteMenuSection, inputChange, sectionIndex, dropDown, addItem}) => {
   
-  const items = data.MenuItems.map(item => <MenuItem data={item} toggleEdit={toggleEdit} />);
+  const items = data.MenuItems.map((item, i) => {
+    return <MenuItem key={i} sectionIndex={sectionIndex} itemIndex={i} 
+            data={item} inputChange={inputChange} dropDown={dropDown} />});
   
   return (
-    <div className="menu-section">
-      <h2>{data.name}</h2>
+    <div className="menu-section form-section">
+      <div className="form-section-header">{data.name}</div> 
+      <button onClick={deleteMenuSection}>Delete Menu Section</button>
       {items}
+      <button onClick={()=>addItem(sectionIndex)}>Add item</button>
     </div>
   );
 };
