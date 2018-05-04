f2000f1c1db33732e1b5df581eda6c09c3384bbc
Fat Pikachu <fatpikachu@Fats-MacBook-Pro.local>
2018-05-04 16:59:29 -0700

    cleanup comments

--- client/src/services/CustomerApiService.js
+++ client/src/services/CustomerApiService.js
@@ -26,13 +26,9 @@ const ApiService = {
     })
   ),
 
-  //userId, email, firstName, lastName, phone, originalEmail, newPW1
   updateCustomerProfile: (id, userName, email, firstName, lastName, phone, originalEmail, password) => 
     AuthService.fetch(`/customers/${id}/profile`, {
       method: 'PATCH',
-      // headers: {
-      //   'content-type': 'application/json'
-      // },
       body: {
         email,
         userName,
@@ -43,8 +39,9 @@ const ApiService = {
         originalEmail
       }
     }),
-    
+
   retrieveOrders: id => Auth.fetch(`/customers/${id}/orders`, { method: 'GET' }),
 };
 
 export default ApiService;
+
