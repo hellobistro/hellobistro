cf05e681f92a5c2ab9f5d1e09aa50601a84cef94
Chris Wohlers <chriswohlers@gmail.com>
2018-05-07 12:30:33 -0700

    Add styling for menu manager.

--- client/src/components/RestaurantApp/MenuSection.jsx
+++ client/src/components/RestaurantApp/MenuSection.jsx
@@ -10,25 +10,34 @@ class MenuSection extends React.Component {
     return (this.props !== nextProps);
   }
 
-  render(){
+  render() {
     const { data, deleteMenuSection, inputChange, sectionIndex, dropDown, addItem, sectionChange } = this.props;
-    const items = data.MenuItems.map((item, i) => {
-      return <MenuItem key={i} sectionIndex={sectionIndex} itemIndex={i} 
-              data={item} inputChange={inputChange} dropDown={dropDown} />});   
+    const items = data.MenuItems.map((item, i) =>  (<MenuItem key={i} sectionIndex={sectionIndex} itemIndex={i} data={item} inputChange={inputChange} dropDown={dropDown} />));
     return (
-      <div className="menu-section form-section">
-        <input className="form-section-header"
+      <div className="form-section">
+        <input
+          className="form-input section-name"
           name="name"
           type="text"
+          placeholder="Add your menu section name here."
           defaultValue={data.name}
-          onChange={(e)=>{sectionChange(sectionIndex, e)}} />
-           <br/>
-        <textarea name="description" type="text"
+          maxLength="255"
+          onChange={(e) => { sectionChange(sectionIndex, e); }}
+        />
+        <br />
+        <textarea
+          className="form-input section-description"
+          name="form-description"
+          type="text"
           defaultValue={data.description}
-          onChange={(e)=>{sectionChange(sectionIndex, e)}} />
+          placeholder="Add a section description here (not required)."
+          maxLength="255"
+          rows="1"
+          onChange={(e) => { sectionChange(sectionIndex, e); }}
+        />
         {/* <button onClick={()=>deleteMenuSection(sectionIndex)}>Delete Menu Section</button> */}
         {items}
-        <button onClick={()=>addItem(sectionIndex)}>Add item</button>
+        <button onClick={() => addItem(sectionIndex)}>Add item</button>
       </div>
     );
   }
