ce3adabf4a71909e0a2d80ae339c006669bb434e
Chris Wohlers <chriswohlers@gmail.com>
2018-05-27 08:49:56 -0700

    Format date for completed items.

--- client/src/components/CustomerApp/OrderHistoryCompleted.jsx
+++ client/src/components/CustomerApp/OrderHistoryCompleted.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import moment from 'moment';
 
 const renderItems = list => list.map(item => <li>{item.name}</li>);
 
 const OrderHistoryCompleted = ({ data }) => (
   <tr key={data.id}>
     <td>{data.id}</td>
-    <td>{data.createdAt}</td>
+    <td>{moment(data.createdAt).format('MMM D, YYYY')}</td>
     <td>{data.Restaurant.name}</td>
     <td>${data.total}</td>
   </tr>
