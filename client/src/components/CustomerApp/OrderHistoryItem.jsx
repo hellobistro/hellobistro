8e0600e100e6c2c6b2710d6b61872f5fbdbecc73
Chris Wohlers <chriswohlers@gmail.com>
2018-05-27 08:42:46 -0700

    Add estimated wait calculation to Orders.

--- client/src/components/CustomerApp/OrderHistoryItem.jsx
+++ client/src/components/CustomerApp/OrderHistoryItem.jsx
@@ -1,18 +1,41 @@
 import React from 'react';
+import moment from 'moment';
 
-const renderItems = list => list.map(item => <li key={item.id}>{item.name}</li>);
-// const consoleWait = list => console.log('List', list[0].prepTime);
-const renderWait = list => list.reduce((a, b) => a.prepTime + b.prepTime, { prepTime: 0 });
+class OrderHistoryItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { currentWait: "null" };
+  }
 
-const OrderHistoryItem = ({ data }) => (
-  <div className="order-history-item">
-    <p><strong>Order at {data.Restaurant.name}</strong></p>
-    <p><strong>Status:</strong> {data.status}</p>
-    <p><strong>Estimated wait:</strong> {renderWait(data.MenuItems)} minutes</p>
-    <p><strong>Price paid:</strong> ${data.total.toFixed(2)}</p>
-    <p><strong>Items ordered:</strong></p>
-    <ul>{renderItems(data.MenuItems)}</ul>
-  </div>
-);
+  renderItems = list => {
+    return list.map(item => <li key={item.id}>{item.name}</li>);
+  }
+
+  renderWait = order => {
+    const orderPrep = order.MenuItems.reduce((a, b) => a.prepTime + b.prepTime, { prepTime: 0 });
+    const currentWait = moment(order.createdAt).add('minutes', orderPrep || 0).diff(moment(), 'minutes')
+    if (currentWait > 0 ) {
+      this.setState({ currentWait: 'Your estimated wait is ' + currentWait + '\u00A0minute(s).' })
+    } else {
+      this.setState({ currentWait: 'Hmmm... Your food should be ready. Check with staff.'})
+    }
+  }
+
+  componentDidMount() {
+    this.renderWait(this.props.data);
+  }
+
+  render() {
+    return (
+      <div className="order-history-item">
+        <p><strong>Order at {this.props.data.Restaurant.name}</strong></p>
+        <p><strong>Status:</strong> {this.state.currentWait}</p>
+        <p><strong>Price paid:</strong> ${this.props.data.total.toFixed(2)}</p>
+        <p><strong>Items ordered:</strong></p>
+        <ul>{this.renderItems(this.props.data.MenuItems)}</ul>
+      </div>
+    )
+  }
+};
 
 export default OrderHistoryItem;
