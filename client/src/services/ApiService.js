6ac0a20686ec79c34037c289ab541ccf80d24d8e
Chris Wohlers <chriswohlers@gmail.com>
2018-05-29 19:54:30 -0700

    Standardize method syntax in ApiService.

--- client/src/services/ApiService.js
+++ client/src/services/ApiService.js
@@ -10,42 +10,25 @@ const ApiService = {
     return AuthService.fetch(`/restaurants/${id}`);
   },
 
-  getMenuData(id) {
-    return AuthService.fetch(`/customers/view/${id}`);
-  },
+  getMenuData: id => AuthService.fetch(`/customers/view/${id}`),
 
-  getAnalytics(id) {
-    return AuthService.fetch(`/data/${id}`);
-  },
+  getAnalytics: id => AuthService.fetch(`/data/${id}`),
 
-  getUserLikes(id) {
-    return AuthService.fetch(`/customers/${id}/ratings`);
-  },
+  getUserLikes: id => AuthService.fetch(`/customers/${id}/ratings`),
 
-  incrementRating(customerId, itemId) {
-    return AuthService.fetch(`/customers/${customerId}/ratings/${itemId}`, {
-      method: 'PUT'
-    });
-  },
+  incrementRating: (customerId, itemId) => AuthService.fetch(`/customers/${customerId}/ratings/${itemId}`, { method: 'PUT' }),
 
-  updateRestaurant(id, formValues) {
-    return AuthService.fetch(`/restaurants/${id}`, {
-      method: 'PUT',
-      body: formValues,
-    });
-  },
+  updateRestaurant: (id, formValues) => { AuthService.fetch(`/restaurants/${id}`, { method: 'PUT', body: formValues }); },
 
-  removeOldMenu: (id) => AuthService.fetch(`/restaurants/sections/items/${id}`, {
-      method: "DELETE"
-    }),
+  removeOldMenu: id => AuthService.fetch(`/restaurants/sections/items/${id}`, { method: 'DELETE' }),
 
   addNewMenuSection: (id, name, description) => AuthService.fetch(`/restaurants/section/${id}`, {
-      method: "POST",
-      body: {
-        name,
-        description
-      }
-    }),
+    method: 'POST',
+    body: {
+      name,
+      description,
+    },
+  }),
 
   addNewMenuItem: (
     id,
@@ -62,37 +45,31 @@ const ApiService = {
     status,
     menuSectionId,
   ) => AuthService.fetch(`/restaurants/menu/${id}`, {
-      method: "POST",
-      body: {
-        name,
-        //description,
-        price,
-        vegan,
-        vegetarian,
-        glutenFree,
-        spicy,
-        image,
-        prepTime,
-        rating,
-        status,
-        menuSectionId
-      }
-    }),
+    method: 'POST',
+    body: {
+      name,
+      price,
+      vegan,
+      vegetarian,
+      glutenFree,
+      spicy,
+      image,
+      prepTime,
+      rating,
+      status,
+      menuSectionId,
+    },
+  }),
 
-  getOpenOrdersForRestaurant: (id) => AuthService.fetch(`/restaurants/${id}/orders/open`, {
-      method: "GET",
+  getOpenOrdersForRestaurant: id => AuthService.fetch(`/restaurants/${id}/orders/open`, { method: 'GET' }),
 
-    }),
-
-  completeOpenOrder: (orderId, customerId) => fetch(`${domain}/restaurants/openorder/${orderId}/${customerId}`, { method: 'PATCH' }),
+  completeOpenOrder: (orderId, customerId) => AuthService.fetch(`${domain}/restaurants/openorder/${orderId}/${customerId}`, { method: 'PATCH' }),
 
   submitOrder: orderData => AuthService.fetch(`/customers/${orderData.CustomerId}/orders`, { method: 'POST', body: orderData }),
+
   findRestaurants: () => AuthService.fetch('/customers/view', { method: 'GET' }),
 
-  getCustomerProfile: id =>
-    AuthService.fetch(`/customers/${id}`, {
-      method: 'GET',
-    }),
+  getCustomerProfile: id => AuthService.fetch(`/customers/${id}`, { method: 'GET' }),
 
   updateCustomerProfile: (
     id,
@@ -117,40 +94,19 @@ const ApiService = {
       },
     }),
 
-  retrieveOrders: id =>
-    AuthService.fetch(`/customers/${id}/orders`, { method: 'GET' }),
+  retrieveOrders: id => AuthService.fetch(`/customers/${id}/orders`, { method: 'GET' }),
 
-  addPaymentMethod: dataObject => AuthService.fetch('/customers/payments', {
-    method: 'POST',
-    body: dataObject,
-  }),
+  addPaymentMethod: dataObject => AuthService.fetch('/customers/payments', { method: 'POST', body: dataObject }),
 
   //patch('/restaurants/:restaurant_id/menu/:item_id'
 
-  updateMenuItem: (restaurantId, itemId, info) => 
-    AuthService.fetch(`/restaurants/${restaurantId}/menu/${itemId}`, {
-      method: 'PATCH',
-      body: info
-    }),
-  
-  updateMenuSection: (restaurantId, menuSection, info) => 
-    AuthService.fetch(`/restaurants/menusection/${restaurantId}/${menuSection}`, {
-      method: 'PATCH',
-      body: info
-    }),
+  updateMenuItem: (restaurantId, itemId, info) => AuthService.fetch(`/restaurants/${restaurantId}/menu/${itemId}`, { method: 'PATCH', body: info }),
 
-  deleteMenuSection: (menuSection) => 
-    AuthService.fetch(`/restaurants/menusection/${menuSection}`, {
-      method: 'DELETE',
-    }),
+  updateMenuSection: (restaurantId, menuSection, info) => AuthService.fetch(`/restaurants/menusection/${restaurantId}/${menuSection}`, { method: 'PATCH', body: info }),
 
-  deletePhoto: (imageKey) => 
-    AuthService.fetch('/restaurants/photo', {
-      method: 'DELETE',
-      body: {
-        imageKey
-      }
-    }),
+  deleteMenuSection: menuSection => AuthService.fetch(`/restaurants/menusection/${menuSection}`, { method: 'DELETE' }),
+
+  deletePhoto: imageKey => AuthService.fetch('/restaurants/photo', { method: 'DELETE', body: { imageKey } }),
 
   fetchUserWidgetData: (restaurantId, customerId) => AuthService.fetch(`/data/customers/${restaurantId}/${customerId}`, { method: 'GET' }),
 
@@ -158,11 +114,7 @@ const ApiService = {
 
   deletePaymentMethod: paymentId => AuthService.fetch(`/customers/payments/${paymentId}`, { method: 'DELETE' }),
 
-  getRestaurantList: (lat, lng) => {
-    return AuthService.fetch(`/customers/restaurantList/${lat}/${lng}`, {
-      method: 'GET',
-    })
-  }
+  getRestaurantList: (lat, lng) => AuthService.fetch(`/customers/restaurantList/${lat}/${lng}`, { method: 'GET' }),
 
 };
 
